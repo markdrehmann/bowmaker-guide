33bb5b241e5fd70d9aba7f658f2af8c90b02f026
markdrehmann <markdrehmann@gmail.com>
2021-11-01 14:36:54 -0700

    Playing with homepage

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          The Bowmaker's Guide
+        </h1>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="drehmann-bows-link"
+          href="https://www.markdrehmannbows.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Mark Drehmann - Bowmaker
         </a>
       </header>
     </div>
