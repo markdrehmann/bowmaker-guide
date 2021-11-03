3830a983911990190e372ca9b845b6162ccfd482
markdrehmann <markdrehmann@gmail.com>
2021-11-03 16:57:19 -0700

    Editing App.css

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>
-          The Bowmaker's Guide
-        </h1>
-        <h2>Going to make a pseudo - L'Archet?</h2>
-        <a
+      {/* <header className="App-header"></header> */}
+      <h1>
+        The Bowmaker's Guide
+      </h1>
+      <h2>Going to make a pseudo - L'Archet?</h2>
+      <a
           className="drehmann-bows-link"
           href="https://www.markdrehmannbows.com/"
           target="_blank"
@@ -16,7 +16,6 @@ function App() {
         >
           Mark Drehmann - Bowmaker
         </a>
-      </header>
     </div>
   );
 }
