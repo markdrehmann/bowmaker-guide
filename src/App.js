784f91869b4f17ebedea06ac00eace479a87781c
markdrehmann <markdrehmann@gmail.com>
2021-11-03 21:47:36 -0700

    Figuring out how to make google drive url work for photos

--- src/App.js
+++ src/App.js
@@ -7,17 +7,21 @@ function App() {
       <h1>
         The Bowmaker's Guide
       </h1>
-      <h2>Going to make a pseudo - L'Archet?</h2>
+      <p>Home Page is a welcome with list of makers beneath. Click on Maker, brings you to maker/:id show page, links within for maker/:id/drawings and maker/:id/photos</p>
       <a
-          className="drehmann-bows-link"
-          href="https://www.markdrehmannbows.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Mark Drehmann - Bowmaker
-        </a>
+        className="drehmann-bows-link"
+        href="https://www.markdrehmannbows.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Mark Drehmann - Bowmaker
+      </a>
+      <img src='https://drive.google.com/uc?export=view&id=1B9hyRJsbBiMuknzhCVtoniczGpBRNn8s' alt='Bow Drawing' className="bow-drawing" />
     </div>
   );
 }
 
+
 export default App;
+
+// https://drive.google.com/uc?export=view&id=   THIS IS THE URL TO USE, NEED TO ADD ID OF PHOTO AT END
