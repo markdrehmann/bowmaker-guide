e50a65046d7fe68579bf8563a23093ef041af68a
markdrehmann <markdrehmann@gmail.com>
2021-11-11 13:10:03 -0800

    CSS on ShowBowmaker

--- src/components/bowmakers/ShowBowmaker.js
+++ src/components/bowmakers/ShowBowmaker.js
@@ -6,9 +6,9 @@ function ShowBowmaker() {
     console.log('showbowmaker', bowmaker)
     return (
       <>
-        <h2>{bowmaker.first_name} {bowmaker.last_name}</h2>
+        <h2 className='bowmaker-name' >{bowmaker.first_name} {bowmaker.last_name}</h2>
         <p>{bowmaker.birth_year} - {bowmaker.year_of_death}</p>
-        
+
       </>
     )
   }
