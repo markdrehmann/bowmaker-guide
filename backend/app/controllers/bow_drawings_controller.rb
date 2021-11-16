95865363ca59048114333b53d3fc0b091f06b562
markdrehmann <markdrehmann@gmail.com>
2021-11-16 14:30:42 -0800

    Added useEffect to BowDrawingsContainer

--- backend/app/controllers/bow_drawings_controller.rb
+++ backend/app/controllers/bow_drawings_controller.rb
@@ -1,2 +1,3 @@
 class BowDrawingsController < ApplicationController
+  
 end
--- src/containers/BowDrawingsContainer.js
+++ src/containers/BowDrawingsContainer.js
@@ -1,9 +1,14 @@
 import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function BowDrawingsContainer() {
   const location = useLocation();
   const bowmaker = location.state.bowmaker
-  console.log('drawings container', bowmaker)
+  
+  useEffect(() => {
+    console.log('using effect')
+  })
+
   return (
     <h2>
       This is the bow drawings container?
