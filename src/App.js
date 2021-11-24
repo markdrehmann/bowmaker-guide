2a952b1f82114314a7e0360b66a60584c3e85d37
markdrehmann <markdrehmann@gmail.com>
2021-11-23 22:18:40 -0800

    handleLoading() added to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Head from './components/Head';
 import BowmakersContainer from './containers/BowmakersContainer';
@@ -8,11 +9,20 @@ import ShowBowmaker from './components/bowmakers/ShowBowmaker';
 import BowDrawingsContainer from './containers/BowDrawingsContainer';
 import BowPhotosContainer from './containers/BowPhotosContainer';
 
-function App() {
+function App(props) {
+  const handleLoading = () => {
+    if (props.loading) {
+      return (
+        <h3>Loading...</h3>
+      )
+    }
+  }
+
   return (
     <div className='App'>
         <Router>
           <Head />
+          {handleLoading()}
           <Routes>
             {/* make a '/' path for homepage with link to makers? */}
             <Route path='/makers' element={<BowmakersContainer />} />
@@ -25,6 +35,12 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    loading: state.loading
+  }
+}
+
+export default connect(mapStateToProps)(App);
 
 // <img src='https://lh3.googleusercontent.com/Tqf-koPPDGUqeqoN7TCLZB-bXvJB1hBaE-yzodK7ehvOFAQTby2POkQeZR36jzGf6y7aETyGLmiRoF08iAPTFi8zHgbkjAmFMYxNo2XAdEJtWvlWSYjJKlFTjomP1x7CD80OLo4Y7rE=w2400' alt='Bow Drawing' className="bow-drawing" />
