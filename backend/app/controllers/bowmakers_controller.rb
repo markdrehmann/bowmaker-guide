57be50b885d981e6a9492cda52ac04375912dbe3
markdrehmann <markdrehmann@gmail.com>
2021-11-25 12:08:13 -0800

    Bowmakers_controller added a create method

--- backend/app/controllers/bowmakers_controller.rb
+++ backend/app/controllers/bowmakers_controller.rb
@@ -3,4 +3,20 @@ class BowmakersController < ApplicationController
     bowmakers = Bowmaker.all
     render json: bowmakers, :except => [:created_at, :updated_at]
   end
+
+  def create
+    bowmaker = Bowmaker.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      birth_year: params[:birth_year],
+      year_of_death: params[:year_of_death],
+      biography: params[:biography],
+      notes: params[:notes]
+    )
+    if bowmaker.save
+      render json: bowmaker
+    else
+      render json: { errors: bowmaker.errors.full_messages}
+    end
+  end
 end
